732bc5ecb9a91ff5c7910fc5c8b0627ff1382840
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-11-14 20:48:19 +0100

    Improved login screen

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,6 @@
+function randFloat(min, max) {
+	return Math.random() * (max - min) + min
+}
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -68,7 +71,7 @@ function initMap() {
 	]);
 
 	// some standard location
-	map.setCenter(new OpenLayers.LonLat(randInt(0, 180), randInt(0, 90)).transform(fromProjection, toProjection), 14);
+	map.setCenter(new OpenLayers.LonLat(/*randInt(0, 180), randInt(0, 90)*/randFloat(-76, -78), randFloat(37, 39)).transform(fromProjection, toProjection), 14);
 }
 
 function updatePosition() {
